d5dfef47b9401d3f78c8617cffe28c5757c9616b
Yashwanth <yashwanthreddy2109@gmail.com>
2024-11-22 21:24:46 +0530

    selecting s,ns,bowl activity

--- app/src/main/java/com/cricketscoringapp/criceasy/SelectingSrNsBowActivity.java
+++ app/src/main/java/com/cricketscoringapp/criceasy/SelectingSrNsBowActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,33 @@ public class SelectingSrNsBowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s_ns_bowler); // Make sure this layout file exists
         updateCurrentActivityInPreferences();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("match_prefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //UI
+        ImageView striker_button = findViewById(R.id.imageView);
+        ImageView non_striker_button = findViewById(R.id.imageView2);
+        ImageView bowler_button = findViewById(R.id.imageView3);
+
+        striker_button.setOnClickListener(view ->{
+            editor.putString("player_type","striker");
+            editor.apply();
+            select_player();
+        });
+
+        non_striker_button.setOnClickListener(view ->{
+            editor.putString("player_type","non_striker");
+            editor.apply();
+            select_player();
+        });
+
+        bowler_button.setOnClickListener(view ->{
+            editor.putString("player_type","bowler");
+            editor.apply();
+            select_player();
+        });
+
     }
     @Override
     protected void onResume() {
@@ -22,7 +51,7 @@ public class SelectingSrNsBowActivity extends AppCompatActivity {
         updateCurrentActivityInPreferences();
     }
 
-    public void select_striker(View view) {
+    public void select_player() {
         // Navigate back to MatchInfoActivity
         Intent intent = new Intent(this, SelectingPlayersActivity.class);
         startActivity(intent);
