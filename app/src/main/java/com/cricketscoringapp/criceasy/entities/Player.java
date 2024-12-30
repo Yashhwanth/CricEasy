66bf03f4a83765338b7335456bc6c839658a72f3
Yashwanth <yashwanthreddy2109@gmail.com>
2024-12-30 13:11:09 +0530

    Player Entity

--- app/src/main/java/com/cricketscoringapp/criceasy/entities/Player.java
+++ app/src/main/java/com/cricketscoringapp/criceasy/entities/Player.java
@@ -1,11 +1,14 @@
 package com.cricketscoringapp.criceasy.entities;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "Player")
 public class Player {
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "playerId")
     public int playerId;
-    public String name; // Player name
+    @ColumnInfo(name = "playerName")
+    public String playerName; // Player name
 }
