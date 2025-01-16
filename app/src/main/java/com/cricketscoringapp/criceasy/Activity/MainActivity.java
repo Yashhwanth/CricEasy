bde010fd65a66b9c3af634457d5623b1f132f4a5
Yashwanth <yashwanthreddy2109@gmail.com>
2025-01-16 14:13:52 +0530

    Teams Fragment:recycler views height set to wrap content

--- app/src/main/java/com/cricketscoringapp/criceasy/Activity/MainActivity.java
+++ app/src/main/java/com/cricketscoringapp/criceasy/Activity/MainActivity.java
@@ -51,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         databaseHelper = new DatabaseHelper(this);
         sharedPreferences = getSharedPreferences("match_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor1 = sharedPreferences.edit();
+        editor1.clear();
+        editor1.apply();
 
         // Initialize UI elements
         Button newMatchButton = findViewById(R.id.newMatchButton);
